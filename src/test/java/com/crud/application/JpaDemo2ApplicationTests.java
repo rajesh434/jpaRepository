073d95fd1ad75dae0b2988093f5b13977e0d858d
rajesh434 <rajeshpaygude000@gmail.com>
2020-04-11 12:03:39 +0530

    initial commit

---
+++ src/test/java/com/crud/application/JpaDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
